9e59afc9202ececde1a5952f8bc1e4400deaece7
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-01-22 10:55:10 +0900

    socket draw debug

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-export const baseURL = process.env.NODE_ENV === "production" ? window.location.origin : "http://localhost:5000";
+export const baseURL = process.env.NODE_ENV === "production" ? window.location.origin : "http://localhost:3000";
 
 const socket = io(baseURL, {
   withCredentials: true,
