0f86ebfdf76d17780a08a33e1a6c9e9f20837c5d
Youjin Jo <youjinjoy@gmail.com>
2024-02-10 17:30:02 +0900

    area vertical chart로 변경

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,6 +1,23 @@
 import React, { useState, useEffect, PureComponent } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+	AreaChart,
+	Area,
+} from "recharts";
 import { logger } from "logger";
+import { useRecoilState } from "recoil";
+import { roomUserState, roomUsersState, scrollYState } from "recoil/atom";
+import socket from "socket";
+import { Button } from "@mui/material";
+
+import { useImmer } from "use-immer";
 
 const original_data = new Array(31).fill(0).map((_, index) => ({
 	page: `${index}`,
@@ -9,10 +26,29 @@ const original_data = new Array(31).fill(0).map((_, index) => ({
 
 let updatedData = {};
 
-function Chart({ scroll }) {
+function Chart() {
+	const [scroll, setScroll] = useRecoilState(scrollYState);
 	const [prevScroll, setPrevScroll] = useState(0);
 	const [data, setData] = useState(original_data);
 	const [count, setCount] = useState(0);
+	const [roomUser, setRoomUser] = useRecoilState(roomUserState);
+	const [roomUsers, setRoomUsers] = useRecoilState(roomUsersState);
+
+	useEffect(() => {
+		const handleUpdateChart = (userData) => {
+			console.log("**Received update-chart event**");
+			const { roomUser, updatedData } = userData;
+			const userNick = roomUser.user.nick;
+			const userUpdatedData = [...updatedData];
+			console.log(userNick, userUpdatedData);
+		};
+
+		socket.on("update-chart", handleUpdateChart);
+
+		return () => {
+			socket.off("update-chart", handleUpdateChart);
+		};
+	}, []);
 
 	useEffect(() => {
 		setPrevScroll(scroll);
@@ -45,31 +81,43 @@ function Chart({ scroll }) {
 		});
 
 		setData(updatedData);
+
+		//request-chart updatedData and user info
+		socket.emit("send-chart", { roomUser, updatedData });
 	}, [scroll]);
 
 	return (
 		// width="25%" height={650} style={{ position: "sticky", top: "20px" }}
-		<ResponsiveContainer>
-			<LineChart
-				layout="vertical"
-				width={400}
-				height={800}
-				data={updatedData}
-				margin={{
-					top: 20,
-					right: 30,
-					left: 20,
-					bottom: 5,
-				}}
-			>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis type="number" />
-				<YAxis dataKey="page" type="category" />
-				<Tooltip />
-				<Legend />
-				<Line dataKey="time" stroke="#8884d8" />
-			</LineChart>
-		</ResponsiveContainer>
+		<div style={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
+			<Button variant="outlined" sx={{ width: "30%" }}>
+				갱신
+			</Button>
+			<ResponsiveContainer>
+				<AreaChart
+					layout="vertical"
+					width={400}
+					height={800}
+					data={updatedData}
+					margin={{
+						top: 20,
+						right: 30,
+						left: 20,
+						bottom: 5,
+					}}
+				>
+					<defs>
+						<linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+							<stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+						</linearGradient>
+					</defs>
+					<XAxis type="number" />
+					<YAxis dataKey="page" type="category" />
+					<CartesianGrid strokeDasharray="3 3" />
+					<Tooltip />
+					<Area type="monotone" dataKey="time" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+				</AreaChart>
+			</ResponsiveContainer>
+		</div>
 	);
 }
 
