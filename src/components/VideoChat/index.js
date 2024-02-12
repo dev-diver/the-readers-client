3580da911acca4f166d9d76d90f514369efb7fc9
RachelHongg <seonwoohong97@gmail.com>
2024-02-12 17:22:08 +0900

    pull 받으려고 commit함

--- src/components/VideoChat/index.js
+++ src/components/VideoChat/index.js
@@ -7,7 +7,7 @@ import { Container, OutButton, StartButton, VideoBox, VideoContainer } from "./s
 import { useRecoilState } from "recoil";
 import { userState } from "recoil/atom";
 
-const APPLICATION_SERVER_URL = process.env.NODE_ENV === "production" ? "" : "https://demos.openvidu.io/";
+const APPLICATION_SERVER_URL = "https://demos.openvidu.io/";
 
 const VideoChat = () => {
 	const [session, setSession] = useState(undefined);
@@ -19,37 +19,23 @@ const VideoChat = () => {
 	const { roomId } = useParams();
 	const [mySessionId, setMySessionId] = useState(roomId);
 	const [myUserName, setMyUserName] = useState(user.id);
-	// const mySessionId = roomId;
-	// const myUserName = user.id;
 
 	const navigate = useNavigate();
 	const OV = new OpenVidu();
 
 	useEffect(() => {
-		console.log("---1---");
 		joinSession();
-		console.log("user", user);
-		console.log("roomId", roomId);
-		console.log("mySessionId", myUserName);
-		console.log("myUserName", myUserName);
-		console.log("session", session);
-		console.log("________subscribers", subscribers);
-		console.log("__________mainStreamManager", mainStreamManager);
-		console.log("_________publisher", publisher);
 	}, [user, roomId]);
 
 	useEffect(() => {
-		console.log("session_______", session);
 		if (!session) return;
 		session.on("streamCreated", (event) => {
-			console.log("<-------------Stream created----------->");
 			console.log("Stream created:", event.stream);
 			const subscriber = session.subscribe(event.stream, undefined);
 			setSubscribers((prevSubscribers) => [...prevSubscribers, subscriber]);
 		});
 
 		session.on("streamDestroyed", (event) => {
-			console.log("<-------------StreamDestroyed----------->");
 			setSubscribers((prevSubscribers) =>
 				prevSubscribers.filter((subscriber) => subscriber !== event.stream.streamManager)
 			);
@@ -61,7 +47,6 @@ const VideoChat = () => {
 
 		const connectToSession = async () => {
 			try {
-				console.log("-----------connectToSession, sesseionId----------", mySessionId);
 				const token = await getToken(mySessionId);
 				await session.connect(token, { clientData: myUserName });
 
@@ -99,10 +84,9 @@ const VideoChat = () => {
 			session.off("streamDestroyed");
 			session.off("exception");
 		};
-	}, [session]);
+	}, [session, subscribers]);
 
 	useEffect(() => {
-		console.log("---2---");
 		const onbeforeunload = (event) => {
 			leaveSession();
 		};
@@ -114,13 +98,13 @@ const VideoChat = () => {
 		};
 	}, []);
 
-	const handleChangeSessionId = (e) => {
-		setMySessionId({ ...mySessionId, mySessionId: e.target.value });
-	};
+	// const handleChangeSessionId = (e) => {
+	// 	setMySessionId({ ...mySessionId, mySessionId: e.target.value });
+	// };
 
-	const handleChangeUserName = (e) => {
-		setMyUserName({ ...myUserName, myUserName: e.target.value });
-	};
+	// const handleChangeUserName = (e) => {
+	// 	setMyUserName({ ...myUserName, myUserName: e.target.value });
+	// };
 
 	const handleMainVideoStream = (stream) => {
 		if (mainStreamManager !== stream) {
@@ -138,13 +122,11 @@ const VideoChat = () => {
 	};
 
 	const joinSession = async () => {
-		console.log("<--------------- joinSession ---------->");
 		const mySession = OV.initSession();
 		setSession(mySession);
 	};
 
 	const leaveSession = () => {
-		// redirect to room lobby
 		if (session) {
 			session.disconnect();
 		}
@@ -166,7 +148,6 @@ const VideoChat = () => {
 				const newVideoDevice = videoDevices.filter((device) => device.deviceId !== currentVideoDevice.deviceId);
 
 				if (newVideoDevice.length > 0) {
-					// Creating a new publisher with a specific videoSource
 					const newPublisher = OV.initPublisher(undefined, {
 						videoSource: newVideoDevice[0].deviceId,
 						publishAudio: true,
@@ -174,7 +155,6 @@ const VideoChat = () => {
 						mirror: true,
 					});
 
-					// Wait for accessAllowed event before unpublishing and publishing
 					newPublisher.once("accessAllowed", async () => {
 						await session.unpublish(mainStreamManager);
 						await session.publish(newPublisher);
@@ -190,75 +170,48 @@ const VideoChat = () => {
 		}
 	};
 
-	// getToken 함수 수정
-	const getToken = async (sessionId) => {
-		console.log("<---------- getToken ---------->");
-		try {
-			// 세션 ID를 사용하여 서버로부터 토큰 요청
-			console.log("------try진입----sessionId", sessionId);
-			const response = await axios.post(
-				`${APPLICATION_SERVER_URL}api/sessions/${sessionId}/connections`,
-				{},
-				{ headers: { "Content-Type": "application/json" } }
-			);
-			console.log("Token", response.data);
-			return response.data; // 토큰 반환
-		} catch (error) {
-			console.error("Error getting token:", error);
-		}
+	const getToken = async () => {
+		const sessionId = await createSession(mySessionId);
+		return await createToken(sessionId);
+	};
+
+	const createSession = async (sessionId) => {
+		console.log("createSession", APPLICATION_SERVER_URL);
+		const response = await axios.post(
+			APPLICATION_SERVER_URL + "api/sessions",
+			{ customSessionId: sessionId },
+			{
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+		return response.data; // The sessionId
+	};
+
+	const createToken = async (sessionId) => {
+		console.log("create Token", APPLICATION_SERVER_URL);
+		const response = await axios.post(
+			APPLICATION_SERVER_URL + "api/sessions/" + sessionId + "/connections",
+			{},
+			{
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+		return response.data; // The token
 	};
 
 	return (
 		<div className="container">
-			{session === undefined ? (
-				<div id="join">
-					<div id="img-div">
-						<img src="resources/images/openvidu_grey_bg_transp_cropped.png" alt="OpenVidu logo" />
-					</div>
-					<div id="join-dialog" className="jumbotron vertical-center">
-						<h1> Join a video session </h1>
-						<form className="form-group" onSubmit={joinSession}>
-							<p>
-								<label>Participant: </label>
-								<input
-									className="form-control"
-									type="text"
-									id="userName"
-									value={myUserName}
-									onChange={handleChangeUserName}
-									required
-								/>
-							</p>
-							<p>
-								<label> Session: </label>
-								<input
-									className="form-control"
-									type="text"
-									id="sessionId"
-									value={mySessionId}
-									onChange={handleChangeSessionId}
-									required
-								/>
-							</p>
-							<p className="text-center">
-								<input className="btn btn-lg btn-success" name="commit" type="submit" value="JOIN" />
-							</p>
-						</form>
-					</div>
-				</div>
-			) : null}
-
 			{session !== undefined ? (
 				<div id="session">
 					<div id="session-header">
 						<h1 id="session-title">mysessionId: {mySessionId}</h1>
-						<input
+						{/* <input
 							className="btn btn-large btn-danger"
 							type="button"
 							id="buttonLeaveSession"
-							onClick={leaveSession} // redirect to room lobby
+							onClick={leaveSession}
 							value="Leave session"
-						/>
+						/> */}
 						<input
 							className="btn btn-large btn-success"
 							type="button"
@@ -266,19 +219,19 @@ const VideoChat = () => {
 							onClick={switchCamera}
 							value="Switch Camera"
 						/>
+						<a href={`/room/${roomId}`}>
+							<OutButton onClick={leaveSession}>Exit!</OutButton>
+						</a>
 					</div>
 
 					{mainStreamManager !== undefined ? (
 						<div id="main-video" className="col-md-6">
-							<UserVideoComponent streamManager={mainStreamManager} />
+							<VideoContainer>
+								<UserVideoComponent streamManager={mainStreamManager} />
+							</VideoContainer>
 						</div>
 					) : null}
 					<div id="video-container" className="col-md-6">
-						{publisher !== undefined ? (
-							<div className="stream-container col-md-6 col-xs-6" onClick={() => handleMainVideoStream(publisher)}>
-								<UserVideoComponent streamManager={publisher} />
-							</div>
-						) : null}
 						{subscribers.map((sub, i) => (
 							<div
 								key={sub.id}
