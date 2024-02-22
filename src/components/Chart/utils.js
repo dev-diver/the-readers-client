f9e7650834e3fba899586b198d0b70419f07b4e3
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-02-23 07:14:36 +0900

    유저 칼러 디버깅

--- src/components/Chart/utils.js
+++ src/components/Chart/utils.js
@@ -1,4 +1,4 @@
 const colors = ["#8884d8", "#82ca9d", "#E69F00", "#ff4b73", "#56B4E9"];
 export function coloringUser(userId) {
-	return colors[(Number(userId) - 1) % colors.length];
+	return colors[(Number(userId) || 1 - 1) % colors.length];
 }
